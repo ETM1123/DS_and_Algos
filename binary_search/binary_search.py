d1615791c0c103eeb14407596c9b62d202194870
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2023-02-14 23:30:38 -0500

    Add Binary Search Content
    Added general notes about binary search in README.md, including locate
    card problem. Additionally created, 4 empty python modules:
    - binary_search.py
    - locate_card_brute_force.py
    - locate_card_optimized.py
    - locate_cards_tests.py

---
+++ binary_search/binary_search.py
@@ -0,0 +1,5 @@
+"""General algorithm for Binary Search"""
+
+
+def binary_search(lo : int, hi : int, condition : callable[[int], str]) -> int:
+    pass
---
+++ binary_search/locate_card_optimized.py
@@ -0,0 +1,4 @@
+"""Optimized solution to locate_card: O(log N)"""
+
+def locate_card_binary_search(cards : list[int], query : int) -> int:
+    pass
